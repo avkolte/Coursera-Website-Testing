685200d585435d508646f96f4eb041c582b19b86
avkolte <anupkolte92@gmail.com>
2020-06-14 12:05:29 +0530

    Improved and added

--- src/main/java/pageObjetcs/LandingPage.java
+++ src/main/java/pageObjetcs/LandingPage.java
@@ -12,17 +12,15 @@ import org.openqa.selenium.WebElement;
 public class LandingPage {
 	
 	 public WebDriver driver;
+
+	String searchbox="//input[@placeholder='What do you want to learn?']";
+	String searchbtn="//button[@class='nostyle search-button']//div[@class='magnifier-wrapper']";
+	String signUpButton="//a[contains(text(),'Join for Free')]";
 	
 	 //public Properties prop;
 	 Properties prop= new Properties();
 	 FileInputStream fis;
 
-	 public void readData() throws IOException
-	 {
-		fis=new FileInputStream("/home/avkolte/eclipse-workspace/coursera-testing/src/main/java/resources/config.properties");
-
-			prop.load(fis);
-	 }
 	 
 	public LandingPage(WebDriver driver) {
 
@@ -30,53 +28,16 @@ public class LandingPage {
 			this.driver=driver;
 			
 		}
-	 
-	
-	public WebElement getLogin1() throws IOException 
-	{
-		
-		readData();
-		String loginbtn1=prop.getProperty("loginbtn1");
-		
-		return driver.findElement(By.xpath(loginbtn1));
-	}
-	
-	public WebElement getEmail() throws IOException
-	{
-		readData();
-		String email=prop.getProperty("email");
-		return driver.findElement(By.xpath(email));
-	}
-	
-	public WebElement getPassword() throws IOException
-	{
-		readData();
-		String password=prop.getProperty("password");
-		return driver.findElement(By.xpath(password));
-	}
-	
-	public WebElement getLogin2() throws IOException 
-	{
-		
-		readData();
-		String loginbtn2=prop.getProperty("loginbtn2");
-		
-		return driver.findElement(By.xpath(loginbtn2));
-	}
 	
 	
 	public WebElement getSearchBox() throws IOException
 	{
-		readData();
-		String searchbox=prop.getProperty("searchbox");
 		
 		return driver.findElement(By.xpath(searchbox));
 	}
 	
 	public WebElement getSearchBtn() throws IOException
 	{
-		readData();
-		String searchbtn=prop.getProperty("searchbtn");
 		
 		return driver.findElement(By.xpath(searchbtn));
 	}
